fd42789eae0812f0cc0df956c683c3cac3b3f1a6
Pratik Thakur <68782953+pratikthakur06@users.noreply.github.com>
2022-09-01 16:24:17 +0530

    Add files via upload

---
+++ FirstLetterPrinter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+public class FirstLetterPrinter
+{
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in); 
+        String input = in.nextLine(); 
+        System.out.println(firstLetterPrinter(input));
+	}
+	static String firstLetterPrinter(String str) { 
+        String res="";
+        String[] words = str.split(" ");
+        for(String word:words) {
+            res+=word.charAt(0);
+        }
+        return res;
+    }
+}
